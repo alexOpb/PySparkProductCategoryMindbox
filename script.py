74b04a57a7a0901530ee05e0c25741795d1796ce
Palienko.an <https://adm-itweb-bitbucket.dns-shop.ru/>
2024-04-13 14:31:21 +0500

    many to many error fix

--- script.py
+++ script.py
@@ -1,17 +1,21 @@
 from pyspark.sql import SparkSession, Row
 
-def get_product_category_pairs(products, categories):
-    return products.join(categories, on="id", how="left_outer").select("name", "category")
+def get_product_category_pairs(products, categories, product_categories):
+    product_category = products.join(product_categories, products.id == product_categories.product_id, how="left_outer")\
+                               .join(categories, product_categories.category_id == categories.id, how="left_outer")\
+                               .select(products.name, categories.category)
+    return product_category
     
 spark = SparkSession.builder.getOrCreate()
 products = spark.createDataFrame(
     [Row(id=1, name='product_ab_1'), 
      Row(id=2, name='product_ac_2'), 
      Row(id=3, name='product_null_3')])
-categories = spark.createDataFrame(
-    [Row(id=1, category='A'), 
-     Row(id=1, category='B'), 
-     Row(id=2, category='A'), 
-     Row(id=2, category='C')])
-
-get_product_category_pairs(products, categories).show()
+categories = spark.createDataFrame([Row(id=1, category='A'), 
+                                    Row(id=2, category='B'), 
+                                    Row(id=3, category='C')])
+product_categories = spark.createDataFrame([Row(product_id=1, category_id=1), 
+                                            Row(product_id=1, category_id=2), 
+                                            Row(product_id=2, category_id=1), 
+                                            Row(product_id=2, category_id=3)])
+get_product_category_pairs(products, categories, product_categories).show()
